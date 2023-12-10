7a68b2583c71ead2a0fac5c4bdd4e49024a462ca
AMRRAGHEB118 <amrragheb118@gmail.com>
2023-12-10 20:01:00 +0200

    compute term frequency

--- term_frequency.py
+++ term_frequency.py
@@ -1,4 +1,7 @@
-def compute_term_frequency(documents):
+from math import ceil
+
+
+def compute_frequency(documents):
     term_frequency = {}
     for doc_id, terms in enumerate(documents, start=1):
         for term in terms:
@@ -8,21 +11,38 @@ def compute_term_frequency(documents):
                 term_frequency[term][f"Doc{doc_id}"] = term_frequency[term].get(f"Doc{doc_id}", 0) + 1
     return term_frequency
 
+def compute_term_frequency(documents):
+    term_frequency = {}
+
+    for doc_id, terms in enumerate(documents, start=1):
+        total_terms = len(terms)
+
+        for term in terms:
+            if term not in term_frequency:
+                term_frequency[term] = {f"Doc{doc_id}": round(terms.count(term) / total_terms, 2)}
+            else:
+                term_frequency[term][f"Doc{doc_id}"] = round(terms.count(term) / total_terms, 2)
+
+    return term_frequency
+
 def display_term_frequency(term_frequency):
     terms = sorted(term_frequency.keys())
     header = ["Term"] + [f"Doc{i}" for i in range(1, 11)]
-    separator = "-" * (sum(len(col) + 6 for col in header) - 1)
+    separator1 = "-" * (sum(len(col) + 6 for col in header) - 1)
+    separator2 = "_" * (sum(len(col) + 6 for col in header) - 1)
 
     print("{:<15}".format(header[0]), end="")
     for col in header[1:]:
         print("{:<8}".format(col), end="")
-    print("\n" + separator)
+    print("\n" + separator1)
 
     for term in terms:
         frequencies = [str(term_frequency[term].get(f"Doc{i}", 0)) for i in range(1, 11)]
         print("{:<15}".format(term), end="")
         for freq in frequencies:
             print("{:<8}".format(freq), end="")
-        print()
+        print("\n")
+    print(separator2)
+    print("\n")
 
 
