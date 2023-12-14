0669dcd405e4b30bbe10b171ec208f3fa921c199
AMRRAGHEB118 <amrragheb118@gmail.com>
2023-12-14 07:46:39 +0200

    make phrase query handle more operators in one query

--- phrase_query.py
+++ phrase_query.py
@@ -1,7 +1,8 @@
 from math import sqrt
 import pandas as pd
-from tf_idf import calculate_tf_weight
 import re
+
+from tf_idf import calculate_tf_weight
 
 
 def process_phrase_query(phrase_query, pos_index):
@@ -11,32 +12,33 @@ def process_phrase_query(phrase_query, pos_index):
             final_set.update(pos_index[word][1].keys())
     return final_set
 
-# Update the process_boolean_query function
+
 def process_boolean_query(boolean_query, pos_index):
     boolean_query = re.sub(r'\s+', ' ', boolean_query)
     boolean_query = boolean_query.strip().lower()
 
-    if ' and ' in boolean_query:
-        terms = boolean_query.split(' and ')
-        results = [process_phrase_query(term, pos_index) for term in terms]
-        final_result = set.intersection(*map(set, results))
-        matching_terms = set(terms)
+    operators = ['and', 'or', 'not']
+    operator_used = None
 
-    elif ' or ' in boolean_query:
-        terms = boolean_query.split(' or ')
-        results = [process_phrase_query(term, pos_index) for term in terms]
-        final_result = set.union(*map(set, results))
-        matching_terms = set(terms)
+    for operator in operators:
+        if f' {operator} ' in boolean_query:
+            operator_used = operator
+            break
 
-    elif ' not ' in boolean_query:
-        terms = boolean_query.split(' not ')
-        if len(terms) == 2:
-            included_docs = process_phrase_query(terms[0], pos_index)
-            excluded_docs = process_phrase_query(terms[1], pos_index)
-            final_result = included_docs - excluded_docs
-            matching_terms = set(terms)
+    if operator_used:
+        terms = boolean_query.split(f' {operator_used} ')
+        results = [process_phrase_query(term, pos_index) for term in terms]
+
+        if operator_used == 'and':
+            final_result = set.intersection(*map(set, results))
+        elif operator_used == 'or':
+            final_result = set.union(*map(set, results))
+        elif operator_used == 'not' and len(terms) == 2:
+            final_result = results[0] - results[1]
         else:
-            raise ValueError("Invalid NOT operator usage")
+            raise ValueError(f"Invalid {operator_used.upper()} operator usage")
+
+        matching_terms = set(terms)
 
     else:
         final_result = process_phrase_query(boolean_query, pos_index)
@@ -45,20 +47,17 @@ def process_boolean_query(boolean_query, pos_index):
     return list(final_result), matching_terms
 
 
-
 def calculate_cosine_similarity(normalized_tf_idf, query_string, tfd, pos_index):
-    if ' and ' in query_string or ' or ' in query_string or ' not ' in query_string:
+    if any(operator in query_string for operator in [' and ', ' or ', ' not ']):
 
         boolean_results, matching_terms = process_boolean_query(query_string, pos_index)
-        
+
         query = pd.DataFrame(index=normalized_tf_idf.index)
         query['tf'] = [1 if x in matching_terms else 0 for x in list(normalized_tf_idf.index)]
 
         if not boolean_results:
             raise ValueError(f'Error: Boolean query "{query_string}" did not match any documents.')
 
-        query = pd.DataFrame(index=normalized_tf_idf.index)
-        query['tf'] = [1 if x in boolean_results else 0 for x in list(normalized_tf_idf.index)]
     else:
         # It's a phrase query
         query = pd.DataFrame(index=normalized_tf_idf.index)
@@ -79,6 +78,7 @@ def calculate_cosine_similarity(normalized_tf_idf, query_string, tfd, pos_index)
 
 def calculate_query_length(phrase_query, tfd):
     return sqrt(sum([x ** 2 for x in tfd['idf'].loc[phrase_query.split()]]))
+
 
 def calculate_final_scores(product, query, phrase_query):
     product2 = product.multiply(query['norm'], axis=0)
